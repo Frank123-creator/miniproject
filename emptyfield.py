4d81af6a4e1f6ae1967768bed6d3f052583d4d45
harish151 <125892721+harish151@users.noreply.github.com>
2024-06-08 18:04:55 +0530

    Create emptyfield.py

---
+++ emptyfield.py
@@ -0,0 +1,16 @@
+def Validate_form(request_form):
+    error1=''
+    error2=''
+    text1=request_form.get("search_query1")
+    text2=request_form.get("search_query2")
+    if not text1 and not text2:
+        error1="*Field is required"
+        error2="*Field is required"
+        
+    elif not text1:
+        error1="*Field is required"
+        
+    elif not text2:
+        error2="*Field is required"
+        
+    return error1,error2
