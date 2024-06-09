6eea983598fae46449c1388a3f0ecd60c0c4307d
harish151 <125892721+harish151@users.noreply.github.com>
2024-06-09 21:19:50 +0530

    Update seleniumrun.py

--- seleniumrun.py
+++ seleniumrun.py
@@ -4,13 +4,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+import time
 
 def search(search_query):
     options = webdriver.ChromeOptions()
     options=Options()
     #options.add_argument('--headless')  # Run in headless mode
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-    driver.get("https://www.myntra.com")
+    driver.get("https://www.gsmarena.com/")
     
 
     # Close the login popup if it appears
@@ -19,17 +20,19 @@ def search(search_query):
         close_button.click()
     except:
         pass
-
+    time.sleep(2)
+    #search_box=driver.find_element(By.XPATH, """//*[@id="container"]/div/div[1]/div/div/div/div/div[1]/div/div[1]/div/div[2]/div[1]/div/div[1]/div/div/div/div/div[1]/a[2]""").click()
     # Enter the search query
-    search_box = driver.find_element(By.XPATH, ".//input[@class='desktop-searchBar']")
+    search_box = driver.find_element(By.XPATH, ".//input[@id='topsearch-text']")
     search_box.send_keys(search_query)
     search_box.send_keys(Keys.ENTER)
+    search_box=driver.find_element(By.XPATH,"""//*[@id="review-body"]/div/ul/li[1]/a""").click()
     #search_box.submit()
 
     # Wait for the results to load and display the titles
     driver.implicitly_wait(5)  # You can increase this if your internet is slow
 
-    products = driver.find_elements(By.XPATH, ".//h3[@class='product-brand']")
+    products = driver.find_elements(By.XPATH, ".//h1[@class='specs-phone-name-title']")
     product_names = [product.text for product in products[:1]]  # Get top 5 results
  
     driver.quit()
