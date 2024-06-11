03186e01a9dc44ccf0d5ba933a00a6b07c8fbc31
harish151 <125892721+harish151@users.noreply.github.com>
2024-06-11 19:31:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from seleniumrun import search
 from emptyfield import Validate_form
+from tabledata import tabledata
 app = Flask(__name__,template_folder='template')
 
 @app.route('/', methods=['GET', 'POST'])
@@ -11,10 +12,11 @@ def index():
          error1,error2=Validate_form(request.form)
          if not error1 and not error2:
             search_query1 = request.form.get('search_query1')
-            result1 = search(search_query1)
             search_query2 = request.form.get('search_query2')
-            result2 = search(search_query2)
-            return render_template('project.html', result1=result1,result2=result2)
+            
+            table_data=tabledata(search_query1,search_query2)
+
+            return render_template('project.html',table_data=table_data)
     return render_template('project.html',error1=error1,error2=error2)
 
 if __name__ == '__main__':
