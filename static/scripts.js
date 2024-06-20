78df5c81e5e30ed3eeae7b7505349b263c6d7112
harish151 <125892721+harish151@users.noreply.github.com>
2024-06-20 22:53:01 +0530

    Update scripts.js

--- static/scripts.js
+++ static/scripts.js
@@ -1,10 +1,12 @@
 function fetchData(){
     document.getElementById('loader').style.display='block';
+    document.getElementById('content').style.display='block';
     document.getElementById('result').innerText='';
     fetch('/')
     .then(Response => Response.json())
     .then(data => {
       document.getElementById('loader').style.display='none';
+      document.getElementById('content').style.display='none';
       document.getElementById('result').innerText=data.message; 
     })
   }
